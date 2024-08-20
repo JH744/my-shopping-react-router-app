f3607bb8c974d566cacfc31a2b732a8049ded0b1
JiHun Kim <dydekffrl@naver.com>
2024-08-21 02:21:05 +0900

    first commit

---
+++ src/page/ProducAll.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProducAll = () => {
+  return <div>ProducAll</div>;
+};
+
+export default ProducAll;
---
+++ src/page/ProductDetail.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProductDetail = () => {
+  return <div>상품리스트페이지</div>;
+};
+
+export default ProductDetail;
